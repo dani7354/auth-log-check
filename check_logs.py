dcb33f4d427cfa53e04d4002088fb2f6882fe792
Daniel Stuhr Petersen <d@stuhrs.dk>
2021-12-28 19:08:14 +0100

    Exclude existing rows from csv file

--- check_logs.py
+++ check_logs.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser, FileType
 from datetime import datetime
-from login_attempt import LoginAttempt, create_from_csv_record, create_csv_record
+from login_attempt import LoginAttempt, create_from_csv_record, create_csv_record, get_csv_header_row
 import os
 import re
 import sys
 
-FAILED_LOGIN = 0
-CONNECTION = 1
+FAILED_LOGIN_KEY = 0
 
+CSV_DELIMITER = ";"
 MONTHS = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12 }
 
 regular_expressions = {
-    FAILED_LOGIN: "^\w{3}\s{1,2}\d{1,2} \d{2}:\d{2}:\d{2} pi-nas sshd\[\d+\]: Failed publickey.+$"
+    FAILED_LOGIN_KEY: "^\w{3}\s{1,2}\d{1,2} \d{2}:\d{2}:\d{2} pi-nas sshd\[\d+\]: Failed publickey.+$"
 }
+
 line_parsing_func = { }
 
 
 def add_parsing_functions():
-    line_parsing_func[FAILED_LOGIN] = parse_failed_login
+    line_parsing_func[FAILED_LOGIN_KEY] = parse_failed_login
 
 
 def parse_arguments():
@@ -92,7 +93,7 @@ def parse_date(line):
     today = datetime.today()
 
     date = line[start_index:end_index].strip()
-    date_splitted = [ x.strip() for x in date.split(' ') ]
+    date_splitted = [ x.strip() for x in date.split(" ") ]
 
     if len(date_splitted) == str_split_count_padded:
         day_no = f"0{date_splitted[2]}"
@@ -107,9 +108,6 @@ def parse_date(line):
         year_no = today.year
 
     return f"{year_no}-{month_no}-{day_no} {date_splitted[-1]}"
-
-
-
 
 
 def parse_failed_login(line):
@@ -127,15 +125,36 @@ def parse_matched_lines(matches):
         parsing_function = line_parsing_func[match[1]]
         login_attempt = parsing_function(match[0])
         login_attempts.append(login_attempt)
-
     return login_attempts
 
 
+def exclude_known_attempts(new_attempts, output_file):
+    if not os.path.isfile(output_file):
+        return
+
+    new_attempts_dct = { a.id: a for a in new_attempts }
+    known_attempts_dct = {}
+    with open(output_file, "r") as output_file:
+        lines = output_file.readlines()[1:]
+
+    for line in lines:
+        attempt  = create_from_csv_record(line, CSV_DELIMITER)
+        known_attempts_dct[attempt.id] = attempt
+
+    for id in  known_attempts_dct.keys():
+        if id in new_attempts_dct:
+            new_attempts.remove(new_attempts_dct[id])
+
+
 def write_to_file(file, login_attempts):
+    if not os.path.isfile(file):
+        with open(file, "w") as output_file:
+            output_file.write(f"{get_csv_header_row(CSV_DELIMITER)}\n")
     with open(file, "a") as output_file:
         for login_attempt in login_attempts:
             line = create_csv_record(login_attempt)
-            output_file.write(line + "\n")
+            output_file.write(f"{line}\n")
+
 
 def main():
     try:
@@ -148,26 +167,41 @@ def main():
         print("Reading regular expressions...")
         regex_patterns = create_regex_patterns()
 
+        error_count = 0
         file_counter = 0
         total_files = len(arguments.log_files)
+        new_attempts = []
         for log in arguments.log_files:
-            file_counter += 1
-            base_log_string = f"File {file_counter} of {total_files}"
+            try:
+                file_counter += 1
+                base_log_string = f"File {file_counter} of {total_files}"
 
-            print(f"{base_log_string}: Reading log file: {log}...")
-            matches = read_log(log, regex_patterns)
-            print(f"{base_log_string}: Found {len(matches)} matches!")
+                print(f"{base_log_string}: Reading log file: {log}...")
+                matches = read_log(log, regex_patterns)
+                print(f"{base_log_string}: Found {len(matches)} matches!")
 
-            print(f"{base_log_string}: Parsing matching lines...")
-            login_attempts = parse_matched_lines(matches)
+                print(f"{base_log_string}: Parsing matching lines...")
+                login_attempts = parse_matched_lines(matches)
 
-            print(f"Writing to {arguments.output_file}...")
-            write_to_file(arguments.output_file, login_attempts)
+                new_attempts.extend(login_attempts)
+                print(f"{base_log_string}: Done!")
 
-            print(f"{base_log_string}: Done!")
+            except Exception as ex:
+                print(f"Error while reading file {log}... {ex}")
+                error_count += 1
 
-        print("Done!")
-        sys.exit(0)
+        print("Excluding existing rows from results...")
+        exclude_known_attempts(new_attempts, arguments.output_file)
+
+        print(f"Writing to {arguments.output_file}...")
+        write_to_file(arguments.output_file, new_attempts)
+
+        if error_count == 0:
+            print("Done!")
+            sys.exit(0)
+        else:
+            print(f"Finished with {error_count} errors!")
+            sys.exit(1)
 
     except Exception as ex:
         print(f"Something went wrong: {ex}")
--- login_attempt.py
+++ login_attempt.py
@@ -1,23 +1,33 @@
 from hashlib import sha256
 
+DEFAULT_CSV_DELIMITER = ";"
+
+def get_csv_header_row(delimiter=DEFAULT_CSV_DELIMITER):
+    return f"id{delimiter}user{delimiter}src_ip{delimiter}src_port{delimiter}date"
+
+
 def create_csv_record(attempt):
     return f"{attempt.id};{attempt.user};{attempt.src_ip};{attempt.src_port};{attempt.datetime}"
 
-def create_from_csv_record(record, delimiter=";"):
+
+def create_from_csv_record(record, delimiter=DEFAULT_CSV_DELIMITER):
     record_split = record.split(delimiter)
     return LoginAttempt(record_split[1],
         record_split[2],
         record_split[3],
-        record_split[4])
+        record_split[4],
+        record_split[0])
+
 
 class LoginAttempt(object):
     def _generate_id(self):
         input_str = f"{self.user}{self.src_ip}{self.src_port}{self.datetime}"
         return sha256(input_str.encode("ascii")).hexdigest()
 
-    def __init__(self, user, src_ip, src_port, datetime):
+
+    def __init__(self, user, src_ip, src_port, datetime, id = None):
         self.user = user
         self.src_ip = src_ip
         self.src_port = src_port
         self.datetime = datetime
-        self.id = self._generate_id()
+        self.id = id if not id is None else self._generate_id()
